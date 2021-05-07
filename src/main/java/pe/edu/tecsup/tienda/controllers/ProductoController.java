46c998cd17fe3e8008b9e474fc84459f127d60ba
Jaime <jgomezm@tecsup.edu.pe>
2021-05-06 20:42:04 -0500

    Update create record

--- src/main/java/pe/edu/tecsup/tienda/controllers/ProductoController.java
+++ src/main/java/pe/edu/tecsup/tienda/controllers/ProductoController.java
@@ -1,5 +1,9 @@
 package pe.edu.tecsup.tienda.controllers;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -7,9 +11,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import pe.edu.tecsup.tienda.entities.Categoria;
@@ -55,6 +64,42 @@ public class ProductoController {
 		
 		return "productos/create";
 	}
+	
+	
+	@PostMapping("/store")
+	public String store(@ModelAttribute Producto producto, 
+						Errors errors, 
+						@RequestParam MultipartFile file,
+						RedirectAttributes redirectAttrs) 
+		throws Exception{
+		
+		logger.info("call store(producto: " + producto + ")");
+		
+		/*
+		if(file != null && !file.isEmpty()) {
+			String filename = System.currentTimeMillis() + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+			producto.setImagen_nombre(filename);
+			if(Files.notExists(Paths.get(STORAGEPATH))){
+		        Files.createDirectories(Paths.get(STORAGEPATH));
+		    }
+			Files.copy(file.getInputStream(), Paths.get(STORAGEPATH).resolve(filename), StandardCopyOption.REPLACE_EXISTING);
+		}*/
+		
+		producto.setCreado(new Date());
+		producto.setEstado(1);
+		
+		// Graba el producto
+		productoService.save(producto);
+		
+		redirectAttrs.addFlashAttribute("message", "Registro guardado correctamente");
+		
+		return "redirect:/productos/";
+	}
+
+	
+	
+	
+	
 	
 	
 	@GetMapping("/delete/{id}")
