a4a49354ee817d1f26f3925bd6da0a7dd5e03828
Jaime <jgomezm@tecsup.edu.pe>
2021-05-06 20:12:29 -0500

    Update

--- src/main/java/pe/edu/tecsup/tienda/controllers/ProductoController.java
+++ src/main/java/pe/edu/tecsup/tienda/controllers/ProductoController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import pe.edu.tecsup.tienda.entities.Producto;
 import pe.edu.tecsup.tienda.services.CategoriaService;
@@ -41,5 +43,17 @@ public class ProductoController {
 	}
 
 	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Long id, RedirectAttributes redirectAttrs) 
+			throws Exception {
+		
+		logger.info("delete(id: " + id + ")");
+		
+		productoService.deleteById(id);
+		
+		redirectAttrs.addFlashAttribute("message", "Registro eliminado correctamente");
+		
+		return "redirect:/productos/";
+	}
 
 }
