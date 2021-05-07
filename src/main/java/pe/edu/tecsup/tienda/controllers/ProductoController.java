6dd598354113630561b6ba959f70bd3af427a79a
Jaime <jgomezm@tecsup.edu.pe>
2021-05-06 19:40:02 -0500

    Update

--- src/main/java/pe/edu/tecsup/tienda/controllers/ProductoController.java
+++ src/main/java/pe/edu/tecsup/tienda/controllers/ProductoController.java
@@ -1,11 +1,16 @@
 package pe.edu.tecsup.tienda.controllers;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pe.edu.tecsup.tienda.entities.Producto;
 import pe.edu.tecsup.tienda.services.CategoriaService;
 import pe.edu.tecsup.tienda.services.ProductoService;
 
@@ -20,5 +25,18 @@ public class ProductoController {
 	
 	@Autowired
 	private ProductoService productoService;
+	
+	
+	@GetMapping("/")
+	public String index(Model model) throws Exception {
+		logger.info("call index()");
+		
+		List<Producto> productos = productoService.findAll();
+		model.addAttribute("productos", productos);
+		
+		return "productos/index";
+	}
+
+	
 
 }
