6124e7c1c8e72e50a9faa6f2524f7fdd2dc72573
Jaime <jgomezm@tecsup.edu.pe>
2021-05-06 19:37:10 -0500

    Update

--- src/main/java/pe/edu/tecsup/tienda/controllers/ProductoController.java
+++ src/main/java/pe/edu/tecsup/tienda/controllers/ProductoController.java
@@ -1,5 +1,24 @@
 package pe.edu.tecsup.tienda.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pe.edu.tecsup.tienda.services.CategoriaService;
+import pe.edu.tecsup.tienda.services.ProductoService;
+
+@Controller
+@RequestMapping("/productos")
 public class ProductoController {
+
+	private Logger logger = LoggerFactory.getLogger(ProductoController.class);
+	
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@Autowired
+	private ProductoService productoService;
 
 }
