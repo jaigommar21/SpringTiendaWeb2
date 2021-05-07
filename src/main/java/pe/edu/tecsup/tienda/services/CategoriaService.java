a464c79291fec7c6812abf1ccf3ea2184832791d
Jaime <jgomezm@tecsup.edu.pe>
2021-05-06 19:27:10 -0500

    Update services

---
+++ src/main/java/pe/edu/tecsup/tienda/services/CategoriaService.java
@@ -0,0 +1,14 @@
+package pe.edu.tecsup.tienda.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import pe.edu.tecsup.tienda.entities.Categoria;
+
+public interface CategoriaService {
+	
+	public List<Categoria> findAll();
+	
+
+}
+
